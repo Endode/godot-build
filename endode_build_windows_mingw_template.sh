f9c3700f278c9b8ea048ac34644a3ea81abad307
Endode <endode9@gmail.com>
2025-12-12 15:56:27 -0500

    Squash merge d3d12-win into master

--- endode_build_windows_mingw_template.sh
+++ endode_build_windows_mingw_template.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export SCRIPT_AES256_ENCRYPTION_KEY=$(cat /media/Other/Vekwrite/projects/VekwriteRPGGame/_not-for-distribution/godot_encryption_key.gdkey)
-export PATH="/home/endode/Programs/llvm-mingw-20250910-msvcrt-ubuntu-22.04-x86_64/bin:$PATH"
-scons platform=windows target=template_release production=yes use_mingw=yes use_llvm=yes use_lto=yes lto=thin d3d12=no
+export PATH="/home/endode/Programs/llvm-mingw-20250910-ucrt-ubuntu-22.04-x86_64/bin:$PATH"
+scons platform=windows target=template_release production=yes use_mingw=yes use_llvm=yes use_lto=yes lto=thin d3d12=yes
 mv bin/godot.windows.template_release.x86_64.llvm.mono.exe bin/godot.windows.template_release.x86_64.exe -f
 mv bin/godot.windows.template_release.x86_64.llvm.mono.console.exe bin/godot.windows.template_release.x86_64.console.exe -f
