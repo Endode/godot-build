65b3798fb839dd4abd0b49689d81f96306d5a076
Endode <endode9@gmail.com>
2026-01-04 18:18:48 -0500

    Disable deprecated features, enabled openxr, disabled JSON RPC
    mmmmmmmm enabling OpenXR for when I get my Steam Frame mmmmmmmm

--- custom.py
+++ custom.py
@@ -1,6 +1,6 @@
 # Generated using https://godot-build-options-generator.github.io
 
-deprecated = "yes"
+# deprecated = "yes"
 minizip = "no"
 module_mono_enabled = "yes"
 module_camera_enabled = "no"
@@ -8,7 +8,7 @@ module_enet_enabled = "no"
 module_hdr_enabled = "no"
 module_mobile_vr_enabled = "no"
 module_multiplayer_enabled = "no"
-module_openxr_enabled = "no"
+module_jsonrpc_enabled = "no"
 module_upnp_enabled = "no"
 module_webxr_enabled = "no"
 module_zip_enabled = "no"
