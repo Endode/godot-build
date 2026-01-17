15b583ecbcbb5c135540c24f801d50402e87330e
Endode <endode9@gmail.com>
2026-01-16 22:11:21 -0500

    msg

--- endode_build_linux_editor.sh
+++ endode_build_linux_editor.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+echo "We now use Docker for the editor build"
+exit 0
 # export SCRIPT_AES256_ENCRYPTION_KEY=$(cat /media/Other/Vekwrite/projects/VekwriteRPGGame/_not-for-distribution/godot_encryption_key.gdkey)
 # FIXME: For some reason, trying to use LLVM right now does not work for me, so for the editor here, I am using GCC. I think something is weird with my system linker or something?? idk, guess I'll wait for an update. ranlib complains about stuff
 # scons platform=linuxbsd target=editor production=yes use_lto=yes lto=thin use_llvm=yes d3d12=no deprecated=yes # If deprecated=no then the mono glue (build_assemblies.py specifically) fails
